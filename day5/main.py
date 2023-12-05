2bcc97085b44bd0810ddd73fa4de94b688e24473
Maxime Baelde <43513562+mbaelde@users.noreply.github.com>
2023-12-05 14:08:57 +0100

    find solution for day 5 part2 but not efficient

--- day5/main.py
+++ day5/main.py
@@ -1,6 +1,6 @@
 import re
 from typing import Dict
-
+from tqdm import tqdm
 
 class RangeMap:
     def __init__(self, maps_txt: str):
@@ -41,9 +41,16 @@ class RangeMap:
             map.update(dict_map)
         return map
 
+    def get_map(self, source_value: int) -> int:
+        destination_value = None
+        for source_range, destination_range in zip(self._source_ranges, self._destination_ranges):
+            if source_value in source_range:
+                destination_value = source_value - source_range.start + destination_range.start
+                break
+        if destination_value is None:
+            destination_value = source_value
+        return destination_value
 
-with open("test.txt", "r") as file:
-    data = file.read()
 
 seeds_key = "seeds: "
 seed_to_soil_map_key = "seed-to-soil map:"
@@ -54,93 +61,97 @@ light_to_temperature_map_key = "light-to-temperature map:"
 temperature_to_humidity_map_key = "temperature-to-humidity map:"
 humidity_to_location_map_key = "humidity-to-location map:"
 
+with open("input.txt", "r") as file:
+    data = file.read()
+
 # extract seeds
 min_seed = 0
 max_seed = int(max(re.findall("\d+", data)))
 data = data.splitlines()
+
+
+# Part 1
 seeds = [
     int(x)
     for x in [d for d in data if seeds_key in d][0].split(seeds_key)[-1].split(" ")
 ]
 
-# extrad seed_to_soil map
-seed_to_soil_map_txt = data[
-    data.index(seed_to_soil_map_key) + 1 : data.index(soil_to_fertilizer_map_key) - 1
-]
-seed_to_soil_range_map = RangeMap(seed_to_soil_map_txt)
-seed_to_soil_map = seed_to_soil_range_map.generate_map(min_seed, max_seed)
-
-# extrad soil_to_fertilizer map
-soil_to_fertilizer_map_txt = data[
-    data.index(soil_to_fertilizer_map_key)
-    + 1 : data.index(fertilizer_to_water_map_key)
-    - 1
-]
-soil_to_fertilizer_range_map = RangeMap(soil_to_fertilizer_map_txt)
-soil_to_fertilizer_map = soil_to_fertilizer_range_map.generate_map(min_seed, max_seed)
-
-# extrad fertilizer_to_water map
-fertilizer_to_water_map_txt = data[
-    data.index(fertilizer_to_water_map_key) + 1 : data.index(water_to_light_map_key) - 1
-]
-fertilizer_to_water_range_map = RangeMap(fertilizer_to_water_map_txt)
-fertilizer_to_water_map = fertilizer_to_water_range_map.generate_map(min_seed, max_seed)
-
-
-# extrad water_to_light map
-water_to_light_map_txt = data[
-    data.index(water_to_light_map_key)
-    + 1 : data.index(light_to_temperature_map_key)
-    - 1
-]
-water_to_light_range_map = RangeMap(water_to_light_map_txt)
-water_to_light_map = water_to_light_range_map.generate_map(min_seed, max_seed)
-
-
-# extrad fertilizer_to_water map
-light_to_temperature_map_txt = data[
-    data.index(light_to_temperature_map_key)
-    + 1 : data.index(temperature_to_humidity_map_key)
-    - 1
-]
-light_to_temperature_range_map = RangeMap(light_to_temperature_map_txt)
-light_to_temperature_map = light_to_temperature_range_map.generate_map(
-    min_seed, max_seed
-)
-
-
-# extrad temperature_to_humidity map
-temperature_to_humidity_map_txt = data[
-    data.index(temperature_to_humidity_map_key)
-    + 1 : data.index(humidity_to_location_map_key)
-    - 1
-]
-temperature_to_humidity_range_map = RangeMap(temperature_to_humidity_map_txt)
-temperature_to_humidity_map = temperature_to_humidity_range_map.generate_map(
-    min_seed, max_seed
-)
-
-
-# extrad humidity_to_location map
-humidity_to_location_map_txt = data[data.index(humidity_to_location_map_key) + 1 :]
-humidity_to_location_range_map = RangeMap(humidity_to_location_map_txt)
-humidity_to_location_map = humidity_to_location_range_map.generate_map(
-    min_seed, max_seed
-)
-
-seeds_locations = {
-    seed: humidity_to_location_map[
-        temperature_to_humidity_map[
-            light_to_temperature_map[
-                water_to_light_map[
-                    fertilizer_to_water_map[
-                        soil_to_fertilizer_map[seed_to_soil_map[seed]]
-                    ]
-                ]
-            ]
-        ]
+def get_seed_location(seed, data):
+    # extrad seed_to_soil map
+    seed_to_soil_map_txt = data[
+        data.index(seed_to_soil_map_key) + 1 : data.index(soil_to_fertilizer_map_key) - 1
     ]
-    for seed in seeds
-}
+    seed_to_soil_range_map = RangeMap(seed_to_soil_map_txt)
+    soil = seed_to_soil_range_map.get_map(seed)
 
+    # extrad soil_to_fertilizer map
+    soil_to_fertilizer_map_txt = data[
+        data.index(soil_to_fertilizer_map_key)
+        + 1 : data.index(fertilizer_to_water_map_key)
+        - 1
+    ]
+    soil_to_fertilizer_range_map = RangeMap(soil_to_fertilizer_map_txt)
+    fertilizer = soil_to_fertilizer_range_map.get_map(soil)
+
+    # extrad fertilizer_to_water map
+    fertilizer_to_water_map_txt = data[
+        data.index(fertilizer_to_water_map_key) + 1 : data.index(water_to_light_map_key) - 1
+    ]
+    fertilizer_to_water_range_map = RangeMap(fertilizer_to_water_map_txt)
+    water = fertilizer_to_water_range_map.get_map(fertilizer)
+
+    # extrad water_to_light map
+    water_to_light_map_txt = data[
+        data.index(water_to_light_map_key)
+        + 1 : data.index(light_to_temperature_map_key)
+        - 1
+    ]
+    water_to_light_range_map = RangeMap(water_to_light_map_txt)
+    light = water_to_light_range_map.get_map(water)
+
+    # extrad fertilizer_to_water map
+    light_to_temperature_map_txt = data[
+        data.index(light_to_temperature_map_key)
+        + 1 : data.index(temperature_to_humidity_map_key)
+        - 1
+    ]
+    light_to_temperature_range_map = RangeMap(light_to_temperature_map_txt)
+    temperature = light_to_temperature_range_map.get_map(light)
+
+    # extrad temperature_to_humidity map
+    temperature_to_humidity_map_txt = data[
+        data.index(temperature_to_humidity_map_key)
+        + 1 : data.index(humidity_to_location_map_key)
+        - 1
+    ]
+    temperature_to_humidity_range_map = RangeMap(temperature_to_humidity_map_txt)
+    humidity = temperature_to_humidity_range_map.get_map(temperature)
+
+    # extrad humidity_to_location map
+    humidity_to_location_map_txt = data[data.index(humidity_to_location_map_key) + 1 :]
+    humidity_to_location_range_map = RangeMap(humidity_to_location_map_txt)
+    location = humidity_to_location_range_map.get_map(humidity)
+
+    return location
+
+seeds_locations = {}
+for seed in seeds:
+    seeds_locations[seed] = get_seed_location(seed, data)
 print(min(seeds_locations.values()))
+
+# Part 2
+seeds = [
+    int(x)
+    for x in [d for d in data if seeds_key in d][0].split(seeds_key)[-1].split(" ")
+]
+
+seeds_ranges = [range(seeds[i], seeds[i]+seeds[i+1]) for i in [x for x in range(len(seeds)) if x%2==0]]
+
+min_location = max_seed
+for seed_range in tqdm(seeds_ranges, desc="seed_range"):
+    for seed in tqdm(seed_range, "seed"):
+        location = get_seed_location(seed, data)
+        min_location = min(min_location, location)
+
+print(min_location)
+#result : 12634632
